c99843eb725c823bc0555bcebca1a8b5e1ea1678
Kwaku Asare <lilkwaku55@yahoo.com>
2021-07-26 01:01:48 -0400

    banner component

---
+++ client/src/components/banner.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Banner() {
+    return (
+        <div className="container">
+            <div className="jumbotron bg-primary text-center">
+                <h1>Brotherman Store</h1>
+                <p>All your food and stuff</p>
+                <p>
+                    
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
